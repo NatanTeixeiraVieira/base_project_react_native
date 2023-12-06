edfd1395d4707ce319d90e86b387edbaec4c1888
Natãn Teixeira Vieira <natanteixeiravieira@gmail.com>
2023-12-06 16:41:14 -0300

    feat: Add get location

--- src/screens/Home/index..tsx
+++ src/screens/Home/index..tsx
@@ -1,5 +1,27 @@
-import { Container } from './styles';
+import { Container, Title } from './styles';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
+import { TouchableOpacity, Text } from 'react-native';
 
 export default function Home() {
-  return <Container>Home</Container>;
+  const handlePosition = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (granted) {
+      const {
+        coords: { latitude, longitude },
+      } = await getCurrentPositionAsync();
+      console.log(latitude, longitude);
+    }
+  };
+
+  return (
+    <Container>
+      <Title>Home</Title>
+      <TouchableOpacity onPress={handlePosition}>
+        <Text>Clique aqui</Text>
+      </TouchableOpacity>
+    </Container>
+  );
 }
